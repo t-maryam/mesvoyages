2a1a08623fc5ce3f40bdf27ea9fa1fc026101181
Maryam <unexpectedparisienne@hotmail.fr>
2025-10-10 17:12:19 +0200

    Phase 4 : plusieurs pages

---
+++ src/Controller/VoyagesController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Description of VoyagesController
+ *
+ * @author maryam
+ */
+class VoyagesController extends AbstractController {
+    #[Route('/voyages', name: 'voyages')]
+    public function index() : Response {
+        return $this->render("pages/voyages.html.twig");
+    }
+}
