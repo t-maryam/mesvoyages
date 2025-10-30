f1b3f5c9cbef41eb389d328bc72bac3fcbf9fb60
Maryam <unexpectedparisienne@hotmail.fr>
2025-10-30 14:29:40 +0100

    Phase 9 : ORM (filtrage des enregistrements)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -39,6 +40,17 @@ class VoyagesController extends AbstractController {
             'visites' => $visites
         ]);
     }
+    
+    #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal')]
+    public function findAllEqual($champ,Request $request) : Response{
+        $valeur = $request->get("recherche");
+        $visites = $this->repository->findByEqualValue($champ, $valeur);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    
 
     
     
